b74cd8d982e647eb55d9bdbac894e5770b89b0ea
Ndongmo fabrice <barosndongmo@yahoo.fr>
2018-09-10 05:54:52 +0100

    general update

---
+++ jar/levels/fullTileset.tsx
@@ -0,0 +1,784 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="fullTileset" tilewidth="32" tileheight="32">
+ <image source="fullTileset.png" width="832" height="800"/>
+ <tile id="24">
+  <properties>
+   <property name="trap" value="1"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="tower" value="1"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="trap" value="1"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="tower" value="1"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="88">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="89">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="104">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="106">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="107">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="108">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="109">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="110">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="112">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="114">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="115">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="116">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="118">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="119">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="120">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="121">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="122">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="124">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="125">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="128">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="154">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="180">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="192">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="193">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="204">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="205">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="206">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="218">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="219">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="230">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="231">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="232">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="234">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="235">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="236">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="237">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="238">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="239">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="240">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="242">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="243">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="244">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="245">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="246">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="247">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="248">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="249">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="250">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="251">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="252">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="254">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="255">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="256">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="257">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="258">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="284">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="310">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="322">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="323">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="336">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="348">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="349">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="360">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="361">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="362">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="364">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="365">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="366">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="367">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="368">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="369">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="370">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="372">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="373">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="374">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="375">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="376">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="377">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="378">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="379">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="380">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="381">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="382">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="384">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="385">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="386">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="387">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="388">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="414">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="440">
+  <properties>
+   <property name="path" value="1"/>
+  </properties>
+ </tile>
+ <tile id="452">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="453">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="464">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="465">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="478">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="479">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="490">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="491">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="494">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="495">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="496">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="497">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="498">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="499">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="500">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="502">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="503">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="504">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="505">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="506">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="507">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="508">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="509">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="510">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="511">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="512">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="514">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="515">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="516">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="517">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="583">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="594">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="595">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="620">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="621">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="624">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="625">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="626">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="627">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="628">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="629">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="630">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="632">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="633">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="636">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="637">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="638">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="639">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="640">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="641">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="642">
+  <properties>
+   <property name="nexus" value="1"/>
+  </properties>
+ </tile>
+ <tile id="644">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="645">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="646">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+ <tile id="647">
+  <properties>
+   <property name="buildable" value="1"/>
+  </properties>
+ </tile>
+</tileset>
